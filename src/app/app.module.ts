0613f6c921e026d96fa722d701e6c520284880d8
jesus <chuyelregio5@yahoo.com>
2017-11-26 21:31:08 -0800

    finishing pipes tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ShortenPipes } from './shorten.pipes';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShortenPipes
+    ShortenPipes,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
