b31abd14edd88037905df46db3ab9449e0005df2
jesus <chuyelregio5@yahoo.com>
2017-11-26 20:54:59 -0800

    working with pipes tutorial

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ShortenPipes } from './shorten.pipes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShortenPipes
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
